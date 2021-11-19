ad99940d9440762aaa78aaf99ddb7ce06ff2356e
AWSMProdigy <StraightAKyle@gmail.com>
2021-11-18 21:26:01 -0500

    Added saveRecord function

--- public/db.js
+++ public/db.js
@@ -8,3 +8,25 @@ request.onupgradeneeded = function (event) {
   });
   BudgetStore.createIndex("statusIndex", "status");
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+
+  request.onerror = function (event) {
+    // log error here
+    console.log(event.error);
+  };
+  
+  function saveRecord(record) {
+    const transaction = db.transaction(["BudgetDB"], "readwrite");
+    const BudgetStore = transaction.objectStore("BudgetDB");
+    BudgetStore.add({ listID: "1", status: "complete" });
+    // create a transaction on the pending db with readwrite access
+    // access your pending object store
+    // add record to your store with add method.
+  }
